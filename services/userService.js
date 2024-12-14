752ee72732f620a9570a7115bfa63d0220d3a5cd
Rohan <rohanrc101@gmail.com>
2024-12-14 18:55:07 +0300

    [FEAT] add a function to search a user by id

--- services/userService.js
+++ services/userService.js
@@ -1,13 +1,19 @@
 const { readUsers } = require("../utils/fileHandler")
 
-module.exports={
-    async findUserEmail(email){
-        const users= readUsers();
-        return users.find(user=>user.email === email)
+module.exports = {
+    async findUserEmail(email) {
+        const users = readUsers();
+        return users.find(user => user.email === email)
     },
 
-    sanitizeUserData(user){
-        const {password,...sanitizedUser} = user
+    sanitizeUserData(user) {
+        const { password, ...sanitizedUser } = user
         return sanitizedUser
+    },
+
+    async findUserIndex(userId) {
+        const users = readUsers()
+        const userIndex = users.findIndex(user => user.id === userId)
+        return userIndex
     }
 }
