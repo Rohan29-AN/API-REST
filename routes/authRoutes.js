e0dd36c0ec95be9d4a415ec4c958f3e0db9c53fc
Rohan <rohanrc101@gmail.com>
2024-12-14 18:46:18 +0300

    [FEAT] add a verification for the login and register routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express= require("express");
 const { register, login } = require("../controllers/authController");
+const { validateInput } = require("../middlewares/validateInput");
+const {loginSchema,registerSchema} = require('../schemas/userSchema')
 const router= express.Router()
 
-router.post('/register', register);
-router.post('/login',login);
+router.post('/register',validateInput(registerSchema), register);
+router.post('/login',validateInput(loginSchema),login);
 
 module.exports= router;
